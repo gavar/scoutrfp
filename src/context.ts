65aee0308c13c33c566c4260c9e22380f6a1258f
Max Stankevich <max.stankevich.me@gmail.com>
2018-12-09 18:48:46 +0200

    feat: app context

---
+++ src/context.ts
@@ -0,0 +1,16 @@
+/**
+ * Simple dependency container.
+ * Explicitly defines set of services available to a application.
+ */
+export interface Context {
+
+}
+
+/**
+ * Create and configure application context.
+ */
+export function configureContext(): Context {
+  return {
+    
+  };
+}
