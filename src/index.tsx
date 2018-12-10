0442fce0eb7e09d73e2b558d7520e29f4d97fd81
Max Stankevich <max.stankevich.me@gmail.com>
2018-12-11 00:47:30 +0200

    fix: events were not working when rendering to fragment

--- src/index.tsx
+++ src/index.tsx
@@ -9,27 +9,19 @@ declare const module: {
 const PRODUCTION = process.env.NODE_ENV === "production";
 const store = configureStore();
 
-// HMR
+// root HTML element
+const root = document.createElement("div");
+root.className = "root";
+
+// initial render
+const {Entry} = require("./entry");
+render(<Entry store={store}/>, root);
+document.body.appendChild(root);
+
+// configuring HMR
 if (!PRODUCTION && module.hot) {
-  console.warn("using HMR for React");
-  // create root element for HMR support
-  const root = document.createElement("div");
-  root.className = "hmr";
-
-  // initial render
-  const {Entry} = require("./entry");
-  render(<Entry store={store}/>, root);
-  document.body.appendChild(root);
-
-  // configuring HMR
   module.hot.accept("./entry.tsx", function () {
     const {Entry} = require("./entry");
     render(<Entry store={store}/>, root);
   });
-} else {
-  // fragment flattens structure of the application root
-  const root = document.createDocumentFragment() as any;
-  const {Entry} = require("./entry");
-  render(<Entry store={store}/>, root);
-  document.body.appendChild(root);
 }
