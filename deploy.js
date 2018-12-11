f02b49efb787e70678cb18ead3046aa401059137
Max Stankevich <max.stankevich.me@gmail.com>
2018-12-11 18:17:06 +0200

    fix: include README / LICENSE in deploy

--- deploy.js
+++ deploy.js
@@ -1,8 +1,17 @@
+const fs = require("fs");
 const path = require("path");
 const ghpages = require("gh-pages");
 
 module.exports = new Promise((resolve, reject) => {
   const basePath = path.join(process.cwd(), "dist");
+  const files = [
+    "LICENSE",
+    "README.md",
+  ];
+
+  for (const file of files)
+    fs.copyFileSync(file, path.join(basePath, file));
+
   const config = {
     message: "CI: deploy",
   };
